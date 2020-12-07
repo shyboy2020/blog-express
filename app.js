16ea682699526bed9687c07fa17a1ecc95484cef
shyboy2020 <759399478@qq.com>
2020-12-07 21:39:26 +0800

    d12.7

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+
 const path = require('path')
+
+const router = require('./router/router')
 
 const app = express()
 
@@ -12,14 +17,25 @@ app.engine('html', require('express-art-template'));
 app.set('view options', {
     debug: process.env.NODE_ENV !== 'production'
 });
+//配置中间件，在挂载路由之前
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
-app.get('/',(req,res) => {
-    res.render('index.html')
-})
+//在 Express 这个框架中，默认不支持Session 和 Cookie
+//需要用到第三方中间件: express-session
+//配置express-session
+app.use(session({
+    //配置加密字符串，在原有的加密基础上和这个字符串拼接加密
+    //为了增加安全性
+    secret: 'gundam',
+    resave: false,
+    saveUninitialized: true
+}))
 
-app.get('/register',(req,res) => {
-    res.render('register.html')
-})
+//挂载路由
+app.use(router)
 
 app.listen(6324,() => {
     console.log('app is running...')
